6938c7b7fa8cf8c0d044d7df12483d7e2b6e51ef
Ngowwa-Ziro <joseph.ngowwa@student.moringaschool.com>
2022-09-28 00:43:49 +0300

    completed assignment

--- src/data/user.js
+++ src/data/user.js
@@ -5,7 +5,7 @@ const user = {
   color: "firebrick",
   links: {
     github: "https://github.com/ZiroNgowwa/",
-    linkedin: "https://www.linkedin.com/learning-login/?lipi=urn%3Ali%3Apage%3Ad_learning_lti%3BhupPolWzS2GMKXV4d2RQ1g%3D%3D&licu&authUUID=Ps6chPu0Ta%2BVPi%2Fv3QsAcw%3D%3D&redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Flti%3FclientKey%3DAQE1ICFBpy8isAAAAYMXBERInG1tBUZsWKRlabTREc8E4lbuvDMz9FFA0_34w3AmhaU%26ltiRedirect%3Dhttps%253A%252F%252Fwww.linkedin.com%252Flearning%252Fleading-yourself%253Fu%253D91981722%26roles%3DAQHgZzdV8WdGMgAAAYMXBERIK61E6jiNW6o0NBH1-pYE1R_GBlzIGNke-5S55QKjphFXLStGSaEEioxn6B28ajkwDo4kyqfxE7dZE8kQvRl9Y8M",
+    linkedin: "https://www.linkedin.com/in/liza/",
   },
 };
 
